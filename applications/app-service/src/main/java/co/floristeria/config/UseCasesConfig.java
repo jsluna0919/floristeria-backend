c836f4396f1192eb325b37ed919c9daa9e5eb17f
jsluna19 <Juan.luna@tau.usbmed.edu.co>
2025-08-23 04:36:41 -0500

    Configuro bean PedidoUseCase

--- applications/app-service/src/main/java/co/floristeria/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/floristeria/config/UseCasesConfig.java
@@ -3,9 +3,11 @@ package co.floristeria.config;
 import co.floristeria.model.arreglofloral.gateways.ArreglofloralRepository;
 import co.floristeria.model.cliente.gateways.ClienteRepository;
 import co.floristeria.model.destinatario.gateways.DestinatarioRepository;
+import co.floristeria.model.pedido.gateways.PedidoRepository;
 import co.floristeria.usecase.arreglofloral.ArreglofloralUseCase;
 import co.floristeria.usecase.cliente.ClienteUseCase;
 import co.floristeria.usecase.destinatario.DestinatarioUseCase;
+import co.floristeria.usecase.pedido.PedidoUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -32,5 +34,13 @@ public class UseCasesConfig {
         @Bean
         public DestinatarioUseCase destinatarioUseCase(DestinatarioRepository destinatarioRepository) {
                 return new DestinatarioUseCase(destinatarioRepository);
+        }
+
+        @Bean
+        public PedidoUseCase pedidoUseCase(PedidoRepository pedidoRepository,
+                                           ClienteRepository clienteRepository,
+                                           ArreglofloralRepository arreglofloralRepository,
+                                           DestinatarioRepository destinatarioRepository) {
+                return new PedidoUseCase(pedidoRepository, clienteRepository, arreglofloralRepository, destinatarioRepository);
         }
 }
